92d507fc4bfb587aec041c4815c36653282befe8
Jeremy Heminger <jeremy@geomail.info>
2017-01-24 13:26:13 -0800

    added more features

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Git-Events
 A simple Drupal 8 module to gather and display GIT commits and issues
+
+## About
+
+This is my first "practical" Drupal 8 module. So, at least at the time of the writing of the readme file there are some legacy files and code from the module tutorials.
+
+99% of the magic is located in src/Plugin/Block/GitEventsBlock.php
+
--- src/Plugin/Block/GitEventsBlock.php
+++ src/Plugin/Block/GitEventsBlock.php
@@ -1,10 +1,17 @@
 <?php
-
 namespace Drupal\git_events\Plugin\Block;
-
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Block\BlockPluginInterface;
 use Drupal\Core\Form\FormStateInterface;
+/**
+ *
+ * GitEventsBlock
+ *
+ * @author Jeremy Heminger <j.heminger@061375.com>
+ * @version 1.0.3
+ *
+ * */
+
 
 /**
  * Provides a 'GIT Events' Block
@@ -16,49 +23,77 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class GitEventsBlock extends BlockBase  implements BlockPluginInterface {
   
+  /**
+   * @param string
+   * */
   private $username;
+  
+  /**
+   * @param array
+   * */
+  private $config;
   
   /**
    * {@inheritdoc}
    */
   public function build() {
     
-    $config = $this->getConfiguration();
-    if (!empty($config['max'])) {
-      $max = $config['max'];
+    $this->config = $this->getConfiguration();
+    
+    if (!empty($this->config['max'])) {
+      $max = $this->config['max'];
     }
     else {
       $max = 10;
     }
 
     // check if the repository user name is set (if not then die)
-    if(empty($config['username'])) {
+    if(empty($this->config['username'])) {
       return array(
       '#markup' => '<p>Please set the GITHUB username in the block settings</p>',
         '#allowed_tags' => ['p']
       ); 
     }else{
       // set the user name
-      $this->username = $config['username'];
+      $this->username = $this->config['username'];
     }
     
     // get the cached events (if exists)
     $json = json_decode(@file_get_contents('modules/git_events/.events'));
 
-    //echo '<pre>';print_r($json );exit();/*REMOVE ME*/
-    
+    switch($this->config['update']) {
+      case '1':
+        $update = 'Y-m-d';
+        break;
+      case '2':
+        $update = 'Y-m-d H';
+        break;
+      case '3':
+        $update = 'Y-m-d H:i:s';
+        break;
+      default:
+        $update = 'Y-m-d';
+    }
     // we only want to update the events once a day (no need to spam Github)
     if(isset($json->timestamp)) {
-      if(date('Y-m-d',strtotime('now')) != date('Y-m-d',$json->timestamp))
+      if(date('Y-m-d',strtotime('now')) != date($update,$json->timestamp))
         $json = $this->getLatestGitEvents();
     }else{
       $json = $this->getLatestGitEvents();  
     }
-    
+    //echo '<pre>';print_r($json );exit();/*REMOVE ME*/
     // update the json file
-    $json->timestamp = strtotime('now');
+    if(is_array($json)) {
+      $json['timestamp'] = strtotime('now');
+    }else{
+      $json->timestamp = strtotime('now');
+    }
     
-    @file_put_contents('modules/git_events/.events',json_encode($json));
+    if(false == @file_put_contents('modules/git_events/.events',json_encode($json)))
+      return array(
+        '#markup' => '<p>There was an issue writing the events cache file</p>',
+        '#allowed_tags' => ['p']
+      ); 
     
     $return = '<div class="git_events">';
 
@@ -67,11 +102,10 @@ class GitEventsBlock extends BlockBase  implements BlockPluginInterface {
       if(isset($item->type)) {
         $i++;
         if($i >= $max)continue;
-        /**
-         * @todo display issues
-         * @todo allow this to be set in the block settings
-         * */
         switch($item->type) {
+            case 'CreateEvent':
+              $return = $this->CreateEvent($item,$return);
+            break;
             case 'PushEvent':
               $return = $this->PushEvent($item,$return);
             break;
@@ -99,18 +133,38 @@ class GitEventsBlock extends BlockBase  implements BlockPluginInterface {
    * @param string $return
    * @return string
    * */
+  private function CreateEvent($item,$return)
+  {
+    if(empty($this->config['showcreate']))return $return;
+    if(false === $this->config['showcreate'])return $return;
+    
+    $return = $this->eventHeader($return);
+    $return .= '<div class="created">'.date('M j, Y h:i A T',strtotime($item->created_at)).'</div>';
+    $return .= '<div>Project: <a href="'.$this->view_url($item->repo->url).'" target="_blank">'.$item->repo->name.'</div>';
+    $return .= '<div>Description: '.substr($item->payload->description,0,150).'...</div>';
+    $return = $this->eventFooter($item,$return);
+    return $return;
+  }
+  /**
+   * @param object $item
+   * @param string $return
+   * @return string
+   * */
   private function PushEvent($item,$return)
   {
+    if(empty($this->config['showcommits']))return $return;
+    if(false === $this->config['showcommits'])return $return;
+    
     $return = $this->eventHeader($return);
-      $return .= '<div class="created">'.date('M j, Y h:i A T',strtotime($item->created_at)).'</div>';
-              $return .= '<div>Project: '.$item->repo->name.'</div>';
-              $return .= '<ul class="commits">';
-              foreach($item->payload->commits as $c) {
-                $return .= '<li>Commit: <a href="'.$this->view_url($c->url).'" target="_blank">'.$c->message.'</a></li>';  
-              }
-              $return .= '</ul>';
-      $return = $this->eventFooter($item,$return);
-      return $return;
+    $return .= '<div class="created">'.date('M j, Y h:i A T',strtotime($item->created_at)).'</div>';
+            $return .= '<div>Project: '.$item->repo->name.'</div>';
+            $return .= '<ul class="commits">';
+            foreach($item->payload->commits as $c) {
+              $return .= '<li>Commit: <a href="'.$this->view_url($c->url).'" target="_blank">'.$c->message.'</a></li>';  
+            }
+            $return .= '</ul>';
+    $return = $this->eventFooter($item,$return);
+    return $return;
   }
   /**
    * @param object $item
@@ -119,13 +173,17 @@ class GitEventsBlock extends BlockBase  implements BlockPluginInterface {
    * */
   private function IssueCommentEvent($item,$return)
   {
+    
+    if(empty($this->config['showissuecomments']))return $return;
+    if(false === $this->config['showissuecomments'])return $return;
+    
     $return = $this->eventHeader($return);
-      $return .= '<div class="created">'.date('M j, Y h:i A T',strtotime($item->payload->issue->updated_at)).'</div>';
-              $return .= '<div>Project: '.$item->repo->name.'</div>';
-              $return .= '<div>Issue: <a href="'.$item->payload->issue->html_url.'" target="_blank">'.$item->payload->issue->title.'</a></div>';  
-              $return .= '<div>Comment: '.substr($item->payload->comment->body,0,150).'...</div>';
-      $return = $this->eventFooter($item,$return);
-      return $return;
+    $return .= '<div class="created">'.date('M j, Y h:i A T',strtotime($item->payload->issue->updated_at)).'</div>';
+            $return .= '<div>Project: '.$item->repo->name.'</div>';
+            $return .= '<div>Issue: <a href="'.$item->payload->issue->html_url.'" target="_blank">'.$item->payload->issue->title.'</a></div>';  
+            $return .= '<div>Comment: '.substr($item->payload->comment->body,0,150).'...</div>';
+    $return = $this->eventFooter($item,$return);
+    return $return;
   }
   /**
    * @param object $item
@@ -134,13 +192,16 @@ class GitEventsBlock extends BlockBase  implements BlockPluginInterface {
    * */
   private function IssuesEvent($item,$return)
   {
+    if(empty($this->config['showissue']))return $return;
+    if(false === $this->config['showissue'])return $return;
+    
     $return = $this->eventHeader($return);
-      $return .= '<div class="created">'.date('M j, Y h:i A T',strtotime($item->payload->issue->updated_at)).'</div>';
-              $return .= '<div>Project: '.$item->repo->name.'</div>';
-              $return .= '<div>Issue: <a href="'.$item->payload->issue->html_url.'" target="_blank">'.$item->payload->issue->title.'</a></div>';  
-              $return .= '<div>'.substr($item->payload->issue->body,0,150).'...</div>';
-      $return = $this->eventFooter($item,$return);
-      return $return;
+    $return .= '<div class="created">'.date('M j, Y h:i A T',strtotime($item->payload->issue->updated_at)).'</div>';
+            $return .= '<div>Project: '.$item->repo->name.'</div>';
+            $return .= '<div>Issue: <a href="'.$item->payload->issue->html_url.'" target="_blank">'.$item->payload->issue->title.'</a></div>';  
+            $return .= '<div>'.substr($item->payload->issue->body,0,150).'...</div>';
+    $return = $this->eventFooter($item,$return);
+    return $return;
   }
   /**
    * @param string $url
@@ -171,7 +232,6 @@ class GitEventsBlock extends BlockBase  implements BlockPluginInterface {
   {
     $return .= '<div class="links">';
     $return .= '<a href="'.$this->view_url($item->repo->url).'" target="_blank" class="fltleft">VIEW PROJECT</a>';
-    //$return .= '<a href="'.$this->view_url($item->repo->url).'" target="_blank" class="fltleft">FORK PROJECT</a>';
     $return .= '</div><div class="clear"></div></div>';
     return $return;
   }
@@ -221,6 +281,35 @@ User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KH
       '#description' => $this->t('The GITHUB repository user name'),
       '#default_value' => isset($config['username']) ? $config['username'] : '',
     );
+    $form['gitevents_block_showcreate'] = array (
+      '#type' => 'checkbox',
+      '#title' => $this->t('Display Create Event'),
+      '#default_value' => isset($config['showcreate']) ? $config['showcreate'] : false,
+    );
+    $form['gitevents_block_showcommits'] = array (
+      '#type' => 'checkbox',
+      '#title' => $this->t('Display Commits'),
+      '#default_value' => isset($config['showcommits']) ? $config['showcommits'] : false,
+    );
+    $form['gitevents_block_showissues'] = array (
+      '#type' => 'checkbox',
+      '#title' => $this->t('Display Issues'),
+      '#default_value' => isset($config['showissues']) ? $config['showissues'] : false,
+    );
+    $form['gitevents_block_showissuecomments'] = array (
+      '#type' => 'checkbox',
+      '#title' => $this->t('Display Issue Comments'),
+      '#default_value' => isset($config['showissuecomments']) ? $config['showissuecomments'] : false,
+    );
+    $form['gitevents_block_update'] = array (
+      '#type' => 'select',
+      '#title' => $this->t('How often should the cache be updated'),
+      '#options' => [
+        '1' => $this->t('Once Daily'),
+        '2' => $this->t('Once Hourly'),
+        '3' => $this->t('Every Refresh')
+      ]
+    );
     return $form;
     
   }
@@ -230,6 +319,11 @@ User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KH
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->setConfigurationValue('max', $form_state->getValue('gitevents_block'));
     $this->setConfigurationValue('username', $form_state->getValue('gitevents_block_username'));
+    $this->setConfigurationValue('showcommits', $form_state->getValue('gitevents_block_showcommits'));
+    $this->setConfigurationValue('showissues', $form_state->getValue('gitevents_block_showissues'));
+    $this->setConfigurationValue('showcreate', $form_state->getValue('gitevents_block_showcreate'));
+    $this->setConfigurationValue('showissuecomments', $form_state->getValue('gitevents_block_showissuecomments'));
+    $this->setConfigurationValue('update', $form_state->getValue('gitevents_block_update'));
   }
   /**
    * {@inheritdoc}
